e173a64baa57c2a4c2395349d2b840a4071f946f
Dennis Walker <denniswalker@me.com>
2022-07-31 12:55:28 -0700

    Removes csm dracut RPMs from image during update.

--- update_box.sh
+++ update_box.sh
@@ -1,22 +1,31 @@
 #!/usr/bin/env bash
 set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-BOX_DIR=$SCRIPT_DIR/images
-STAGE_DIR=$BOX_DIR/temp
+STAGE_DIR=$SCRIPT_DIR/libvirt_host/images
 BOOT_DIR=$SCRIPT_DIR/libvirt_host/boot
-mkdir -p $STAGE_DIR
-mkdir -p $BOOT_DIR
-
 BRANCH="${1:-main}"
 IMAGE_NAME=box.img
 BOX_NAME=k8s_ncn
-
 if [[ -z "${ARTIFACTORY_USER}" || -z "${ARTIFACTORY_TOKEN}" ]]; then
     echo "Missing authentication information for image download. Please set ARTIFACTORY_USER and ARTIFACTORY_TOKEN environment variables."
     exit 1
 fi
 
+function purge_old_assets() {
+    echo "Destroying related VM and previous artifacts..."
+    cd $SCRIPT_DIR/k8s_ncn
+    vagrant destroy -f || true
+    cd $OLDPWD
+    rm -rf $STAGE_DIR
+    rm -rf $BOOT_DIR
+}
+[[ $1 == "--purge" || $2 == "--purge" ]] && purge_old_assets
+mkdir -p $STAGE_DIR
+mkdir -p $BOOT_DIR
+
+FREE_DISK_SPACE=$(df -kh . | tail -n1 | awk '{print $4}')
 echo "This operation requires about 40GB of free hard drive space. If you see write errors, please check that you have free capacity."
+echo "You current have ${FREE_DISK_SPACE} of free disk space."
 
 # Determine correct version of image and begin downloading.
 echo "Referencing image for the head of branch $BRANCH."
@@ -50,13 +59,18 @@ cat <<-EOF > $STAGE_DIR/metadata.json
     "description": "This box contains a libvirt qcow2 image for testing the CSM K8s NCN image.",
     "provider": "libvirt",
     "format": "qcow2",
-    "virtual_size": 25
+    "virtual_size": 40
 }
 EOF
+
+echo "Removing CSM dracut scripts from image..."
+cd $SCRIPT_DIR/libvirt_host
+vagrant ssh -c "sudo virt-customize -a /vagrant/images/box.img --run-command 'zypper -n remove dracut-metal-dmk8s dracut-metal-luksetcd dracut-metal-mdsquash'"
+cd $OLDPWD
 
 # sudo chown $(whoami):admin $STAGE_DIR/$IMAGE_NAME
 echo "Creating a tarball of Vagrant assets. This may take a 5-10 minutes without console feedback."
 cd $STAGE_DIR
 tar cvzf ${BOX_NAME}.box metadata.json Vagrantfile $IMAGE_NAME
 cd $OLDPWD
-vagrant box add --name ${BOX_NAME} $STAGE_DIR/${BOX_NAME}.box
+vagrant box add --name ${BOX_NAME} $STAGE_DIR/${BOX_NAME}.box --force
