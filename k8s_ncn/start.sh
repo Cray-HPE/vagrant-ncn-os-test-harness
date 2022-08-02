bd88428d5093e4f3d036b90916e67222a6f29800
Dennis Walker <denniswalker@me.com>
2022-08-02 07:45:12 -0600

    Auto-destroys ncn image when starting it.

--- k8s_ncn/start.sh
+++ k8s_ncn/start.sh
@@ -2,6 +2,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 # ssh-keygen -R 192.168.56.4
+vagrant destroy -f || true
 [[ $(uname) == "Darwin" && -d /Applications/VNC\ Viewer.app ]] && \
     nohup sleep 5 && /Applications/VNC\ Viewer.app/Contents/MacOS/vncviewer 192.168.56.4 &
 vagrant up --provider=libvirt
