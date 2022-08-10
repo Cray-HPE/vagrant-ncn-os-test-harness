3c59b245a8ce69fcc2125acc25a5bed2c390b490
Dennis Walker <denniswalker@me.com>
2022-08-10 06:15:09 -0600

    Adds help to update_box.sh script.

--- update_box.sh
+++ update_box.sh
@@ -9,19 +9,59 @@ BRANCH="${1:-main}"
 IMAGE_NAME=box.img
 BOX_NAME=k8s_ncn
 
+function display_disk_capacity() {
+    FREE_DISK_SPACE=$(df -kh . | tail -n1 | awk '{print $4}')
+    echo "This operation requires about 40GB of free hard drive space. If you see write errors, please check that you have free capacity."
+    echo "------ You currently have ${FREE_DISK_SPACE} of free disk space. ------"
+}
+
+function help() {
+    cat <<EOH | sed -e 's/^    //';
+    Summary: 
+    The update_box.sh script determines the latest version of the kubernetes image for a given CSM release.
+    It then downloads it from Artifactory and builds a Vagrant box. Lastly, it deletes your k8s_ncn vm
+    and associated volume so that your next boot uses the new image.
+
+    Requirements:
+    - The libvirt_host VM must be up in order to perform the necessary modifications to the image.
+      ----- The libvirt_host is $(cd ./libvirt_host && vagrant status | grep virtualbox | grep -v "\n" | sed -e 's/^default                   //')
+    - $(display_disk_capacity)
+
+    Four environment variables are expected for credentialing:
+    - ARTIFACTORY_USER and ARTIFACTORY_TOKEN are necessary to gain access to Artifactory for image download.
+    - VAGRANT_NCN_USER and VAGRANT_NCN_PASSWORD are necessary to populate ssh credentials into the image.
+
+    Args:
+    - [optional, CSM version], e.g. "1.3" determines which CSM version you want to build the image from.
+      Right now, this must be the first argument. If unspecified, will default to "1.3".
+    - "--from-existing" skips the Artifactory download in case you are just modifying the existing image.
+    - "--help" displays this message.
+EOH
+}
+
 declare -A ARGS=$@
 [[ ! $ARGS[*] =~ '--from-existing' ]]; FROM_EXISTING=$? # Skips downloading new artifacts.
-[[ ! $ARGS[*] =~ '--no-purge' ]]; NO_PURGE=$? # Skips deleting existing artifacts, vm, and image.
+[[ $ARGS[*] =~ '--help' ]] && help && exit 0;
 
 if [[ -z "${ARTIFACTORY_USER}" || -z "${ARTIFACTORY_TOKEN}" ]]; then
     echo "Missing authentication information for image download. Please set ARTIFACTORY_USER and ARTIFACTORY_TOKEN environment variables."
+    help
     exit 1
 fi
 
 if [[ -z "${VAGRANT_NCN_USER}" || -z "${VAGRANT_NCN_PASSWORD}" ]]; then
     echo "Missing authentication information for ssh. Please set VAGRANT_NCN_USER and VAGRANT_NCN_PASSWORD environment variables."
+    help
     exit 1
 fi
+
+# Refresh ssh key in case libvirt_host has been recreated.
+ssh-keygen -R 192.168.56.4
+ssh-keyscan 192.168.56.4 >>~/.ssh/known_hosts
+
+cd $SCRIPT_DIR/libvirt_host
+vagrant up || $(echo "The libvirt_host must be up." && help && exit 1)
+cd $OLDPWD
 
 function purge_old_assets() {
     echo "Destroying related VM and previous artifacts..."
@@ -31,17 +71,15 @@ function purge_old_assets() {
     cd $SCRIPT_DIR/libvirt_host
     vagrant ssh -c "sudo virsh vol-delete k8s_ncn_vagrant_box_image_0_box.img --pool default"
     cd $OLDPWD
-    [[ $FROM_EXISTING == 0 ]] && rm -rf $STAGE_DIR
-    [[ $FROM_EXISTING == 0 ]] && rm -rf $BOOT_DIR
+    rm -rf $STAGE_DIR
+    rm -rf $BOOT_DIR
 }
-[[ $NO_PURGE == 1 ]] || purge_old_assets
+[[ $FROM_EXISTING == 1 ]] || purge_old_assets
 
 mkdir -p $STAGE_DIR
 mkdir -p $BOOT_DIR
 
-FREE_DISK_SPACE=$(df -kh . | tail -n1 | awk '{print $4}')
-echo "This operation requires about 40GB of free hard drive space. If you see write errors, please check that you have free capacity."
-echo "You current have ${FREE_DISK_SPACE} of free disk space."
+display_disk_capacity
 
 # Determine correct version of image and begin downloading.
 if [[ $FROM_EXISTING == 0 ]]; then
@@ -83,7 +121,12 @@ EOF
 
 echo "Removing CSM dracut scripts from image..."
 cd $SCRIPT_DIR/libvirt_host
-vagrant ssh -c "sudo virt-customize -a /vagrant/images/box.img --root-password password:${VAGRANT_NCN_PASSWORD} --run-command 'zypper -n remove dracut-metal-dmk8s dracut-metal-luksetcd dracut-metal-mdsquash'"
+vagrant ssh -- -t <<-EOS
+    sudo virt-customize \
+        -a /vagrant/images/box.img \
+        --root-password password:${VAGRANT_NCN_PASSWORD} \
+        --run-command 'zypper -n remove dracut-metal-dmk8s dracut-metal-luksetcd dracut-metal-mdsquash'
+EOS
 cd $OLDPWD
 
 # sudo chown $(whoami):admin $STAGE_DIR/$IMAGE_NAME
