c55cba8616d66f178096794ce7e3d2d3e7029cae
Dennis Walker <denniswalker@me.com>
2022-08-10 08:57:33 -0600

    Removes host forward of libvirt_host vnc port.

--- mac_one_time_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-brew install vagrant virtualbox vnc-viewer
-# Download patch fix for setting up NFS folders on mac.
-sudo curl -o /opt/vagrant/embedded/gems/2.2.19/gems/vagrant-2.2.19/plugins/hosts/darwin/cap/path.rb https://raw.githubusercontent.com/hashicorp/vagrant/42db2569e32a69e604634462b633bb14ca20709a/plugins/hosts/darwin/cap/path.rb
-
-vagrant plugin install vagrant-libvirt
