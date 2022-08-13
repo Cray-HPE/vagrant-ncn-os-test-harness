4c538442102808cfe6e30beeaf40d22d762e7cbb
Dennis Walker <denniswalker@me.com>
2022-08-13 10:58:59 -0600

    Makes libvirthost configuration idempotent.

--- libvirt_host/configure.sh
+++ libvirt_host/configure.sh
@@ -11,19 +11,29 @@ virt-host-validate
 mkdir -p /root/.ssh
 cp /home/vagrant/.ssh/authorized_keys /root/.ssh/
 
-virsh net-start default
-virsh net-autostart default
+if [[ ! $(virsh net-list | grep default | grep active) ]]; then
+    virsh net-start default
+    virsh net-autostart default
+fi
 
-virsh pool-define-as vagrant_images dir - - - - "/vagrant/images"
-virsh pool-start vagrant_images
-virsh pool-autostart vagrant_images
+if [[ ! $(virsh pool-list | grep default | grep active) ]]; then
+    virsh pool-define-as default dir - - - - "/vagrant/images"
+    virsh pool-start default
+    virsh pool-autostart default
+fi
 
-echo "/vagrant/guest_mount *(rw,sync,insecure,root_squash,no_subtree_check,fsid=25)" >> /etc/exports
+if [[ ! $(cat /etc/exports | grep guest_mount) ]]; then
+    echo "/vagrant/guest_mount *(rw,sync,insecure,root_squash,no_subtree_check,fsid=25)" >> /etc/exports
+fi
+
+mkdir -p /vagrant/guest_mount
 systemctl enable nfs-server
 systemctl start nfs-server
 
+rm -rf /srv/www/htdocs
+ln -s /vagrant/htdocs /srv/www/htdocs
 systemctl enable nginx
 systemctl start nginx
 
-rm -rf /srv/www/htdocs
-ln -s /vagrant/htdocs /srv/www/htdocs
+systemctl enable vboxadd-service
+systemctl start vboxadd-service
