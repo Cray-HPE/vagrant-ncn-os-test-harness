13822bfadad228d4bfdd1baa7799360f7b8404f8
Dennis Walker <denniswalker@me.com>
2022-08-15 06:56:49 -0600

    Enables mac sudoers population for NFS mount creation.

--- scripts/mac_one_time_setup.sh
+++ scripts/mac_one_time_setup.sh
@@ -6,9 +6,9 @@ vagrant plugin install vagrant-libvirt
 sudo curl -o /opt/vagrant/embedded/gems/2.2.19/gems/vagrant-2.2.19/plugins/hosts/darwin/cap/path.rb https://raw.githubusercontent.com/hashicorp/vagrant/42db2569e32a69e604634462b633bb14ca20709a/plugins/hosts/darwin/cap/path.rb
 
 # Eliminates the need to type your password to configure NFS mounts.
-# sudo cat <<-EOF > /etc/sudoers.d/vagrant.conf
-# Cmnd_Alias VAGRANT_EXPORTS_ADD = /usr/bin/tee -a /etc/exports
-# Cmnd_Alias VAGRANT_NFSD = /sbin/nfsd restart
-# Cmnd_Alias VAGRANT_EXPORTS_REMOVE = /usr/bin/sed -E -e /*/ d -ibak /etc/exports
-# %admin ALL=(root) NOPASSWD: VAGRANT_EXPORTS_ADD, VAGRANT_NFSD, VAGRANT_EXPORTS_REMOVE
-# EOF
+sudo cat <<-EOF > /etc/sudoers.d/vagrant.conf
+Cmnd_Alias VAGRANT_EXPORTS_ADD = /usr/bin/tee -a /etc/exports
+Cmnd_Alias VAGRANT_NFSD = /sbin/nfsd restart
+Cmnd_Alias VAGRANT_EXPORTS_REMOVE = /usr/bin/sed -E -e /*/ d -ibak /etc/exports
+%admin ALL=(root) NOPASSWD: VAGRANT_EXPORTS_ADD, VAGRANT_NFSD, VAGRANT_EXPORTS_REMOVE
+EOF
