ee1191d9eee047fbb879e69a4c19f6d2da425d9a
Dennis Walker <denniswalker@me.com>
2022-08-28 09:18:49 -0600

    Removes the need for vagrant-libvirt on host.

--- scripts/mac_one_time_setup.sh
+++ scripts/mac_one_time_setup.sh
@@ -4,14 +4,15 @@ brew install vagrant virtualbox vnc-viewer ngrok
 
 #TODO: symlink eula to remove error message.
 ln -s /Applications/VNC\ Viewer.app/Contents/MacOS/vncviewer /usr/local/bin/vncviewer
-vagrant plugin install vagrant-libvirt vagrant-env
+vagrant plugin install vagrant-env
 # Download patch fix for setting up NFS folders on mac.
-sudo curl -o /opt/vagrant/embedded/gems/2.2.19/gems/vagrant-2.2.19/plugins/hosts/darwin/cap/path.rb https://raw.githubusercontent.com/hashicorp/vagrant/42db2569e32a69e604634462b633bb14ca20709a/plugins/hosts/darwin/cap/path.rb
+[[ $(vagrant --version) == "Vagrant 2.2.19" ]] && \
+    sudo curl -o /opt/vagrant/embedded/gems/2.2.19/gems/vagrant-2.2.19/plugins/hosts/darwin/cap/path.rb https://raw.githubusercontent.com/hashicorp/vagrant/42db2569e32a69e604634462b633bb14ca20709a/plugins/hosts/darwin/cap/path.rb
 
-# Eliminates the need to type your password to configure NFS mounts.
+# Supposedly eliminates the need to type your password to configure NFS mounts per Vagrant docs.
 sudo cat <<-EOF > /etc/sudoers.d/vagrant.conf
 Cmnd_Alias VAGRANT_EXPORTS_ADD = /usr/bin/tee -a /etc/exports
 Cmnd_Alias VAGRANT_NFSD = /sbin/nfsd restart
 Cmnd_Alias VAGRANT_EXPORTS_REMOVE = /usr/bin/sed -E -e /*/ d -ibak /etc/exports
-%admin ALL=(root) NOPASSWD: VAGRANT_EXPORTS_ADD, VAGRANT_NFSD, VAGRANT_EXPORTS_REMOVE
+%admin ALL=(ALL) NOPASSWD: VAGRANT_EXPORTS_ADD, VAGRANT_NFSD, VAGRANT_EXPORTS_REMOVE
 EOF
